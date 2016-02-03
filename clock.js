8d2b3f37d15f5860b1d12003f7a3207ef89f20c8
romil rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-02-03 11:07:06 +0200

    six digit clock writeout

--- clock.js
+++ clock.js
@@ -1,1 +1,31 @@
- 
+ //ootan kuni leht on leatud
+ window.onload = function(){
+
+   var clock = document.getElementById('clock');
+
+   writeDate();
+
+ };
+
+//võtab aja ja kirjutab #clock elemendi sisse
+function writeDate(){
+
+  var today = new Date();
+
+  var hours = addZeroBefore(today.getHours());
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  seconds = addZeroBefore(seconds);
+
+  //muudan #clock elemendi htmli
+  clock.innerHTML = hours + ':' + addZeroBefore(minutes) + ':' + seconds;
+}
+
+function addZeroBefore(number){
+
+  if(number < 10){
+    number = "0" + number;
+  }
+
+  return number;
+}
